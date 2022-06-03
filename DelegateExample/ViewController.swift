cf2d8e55ac963d34c413cd97b9ffbfbfd0845ec3
Mert Gaygusuz <62473566+mertgaygusuz@users.noreply.github.com>
2022-06-03 22:07:34 +0300

    Update ViewController.swift

--- DelegateExample/ViewController.swift
+++ DelegateExample/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var txtKullaniciAdi: UITextField!
+    
+    @IBOutlet weak var txtSifre: UITextField!
+    
+    @IBOutlet weak var lblResult: UILabel!
+    
+    @IBOutlet weak var lblPassword: UILabel!
+    
+    @IBOutlet weak var btnDelete: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        txtKullaniciAdi.delegate = self
+        txtSifre.delegate = self
     }
-
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        
+        if textField.tag == 1{
+            lblResult.text = "Kullanıcı adınız : \(textField.text!)"
+        }
+        else if textField.tag == 2{
+            lblPassword.text = "Parolanız : \(textField.text!)"
+        }
+        return true
+    }
+    
+    
+    @IBAction func btnDelete(_ sender: Any) {
+        lblResult.text = ""
+        lblPassword.text = ""
+        txtKullaniciAdi.text = nil
+        txtSifre.text = nil
+        
+    }
+    
 }
 
